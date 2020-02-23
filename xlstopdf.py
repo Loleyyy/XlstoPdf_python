dad4d4cc5981d8fc85033d04a3294fd4071eab76
Loleyyy <36820892+Loleyyy@users.noreply.github.com>
2020-02-23 16:44:17 +0100

    Update xlstopdf.py

--- xlstopdf.py
+++ xlstopdf.py
@@ -2,9 +2,9 @@ import win32com.client
 from pywintypes import com_error
 
 
-# Path to original excel file
+# Lien du fichier xls
 WB_PATH = r'C:\Users\loleyy\Documents\Taff_python\test.xlsx'
-# PDF path when saving
+# Lien du fichier pdf
 PATH_TO_PDF = r'C:\Users\loleyy\Documents\Taff_python\test.pdf'
 
 excel = win32com.client.Dispatch("Excel.Application")
@@ -12,7 +12,7 @@ excel.Visible = False
 try:
     print('Start conversion to PDF')
     wb = excel.Workbooks.Open(WB_PATH)
-    # Specify the sheet you want to save by index. 1 is the first (leftmost) sheet.
+    # selection du nombre d'index a sauvegarder au moment du reverse
     ws_index_list = [1]
     wb.WorkSheets(ws_index_list).Select()
     wb.ActiveSheet.ExportAsFixedFormat(0, PATH_TO_PDF)
